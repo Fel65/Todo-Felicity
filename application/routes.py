03677b9ea5c9cb3052f6a8c3eb501e79aefe613f
Fel65 <93129277+Fel65@users.noreply.github.com>
2021-12-10 15:28:00 +0000

    Feature 1/1 create tasks (#7)
    * saving progress
    
    * adding create task feature
    
    Co-authored-by: Ubuntu <Felicity@vm2.k0hnc530m3ke3o3r0gxykoidic.bx.internal.cloudapp.net>

--- application/routes.py
+++ application/routes.py
@@ -1,15 +1,24 @@
 from application import app, db
 from application.models import Tasks
-from flask import render_template
+from flask import render_template, request,url_for, redirect
+from application.forms import TaskForm
 
 @app.route('/')
 def home():
     all_tasks = Tasks.query.all()
     return render_template("index.html", all_tasks = all_tasks, title="Home")
 
-@app.route('/create_task')
+
+@app.route('/create_task', methods= ["GET","POST"])
 def create_task():
-    new_task = Tasks(description="New Task")
-    db.session.add(new_task)
-    db.session.commit()
-    return f"Task added with ID {new_task.id}"
+    form = TaskForm()
+    
+    if request.method == "POST":
+    
+        new_task = Tasks(description=form.description.data)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("create_task.html", title="Add a Task", form=form)
+
+
